c7bf64b50c7fdbcee09bed32cd30877decdf0889
Eluvatar <drstadler@gmail.com>
2015-10-09 06:45:47 -0400

    GPLed

--- zombie_scan.py
+++ zombie_scan.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python2.7
+#   Simple module to scan a nation for zombies
+#   Copyright (C) 2013-2014 Eluvatar
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from parser.api import request as api_request
 from time import struct_time
 def scan_nation(nat):
